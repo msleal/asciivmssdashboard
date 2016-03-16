acc17d2c293632cd30d204610b5791a98c83099a
Marcelo Leal <msl@eall.com.br>
2016-03-16 15:19:31 +0000

    Still working on the cleanup of the PROMPT window...

--- includes/azure.py
+++ includes/azure.py
@@ -321,13 +321,16 @@ def get_cmd(access_token, run_event, window_information, panel_information):
 			curs_set(True);
 			echo();
 			#Clear the old command from our prompt line...
-			wmove(window_information['cmd'], 1, 5); wclrtoeol(window_information['cmd']); box(window_information['cmd']);
+			wmove(window_information['cmd'], 1, 5); wclrtoeol(window_information['cmd']);
+			box(window_information['cmd']);
 			draw_line(window_information['cmd'], 0, 62, 1, ACS_URCORNER);
 			draw_line(window_information['cmd'], 0, 63, 1, ACS_ULCORNER);
 			draw_line(window_information['cmd'], 1, 62, 2, ACS_VLINE);
 			draw_line(window_information['cmd'], 2, 62, 1, ACS_LRCORNER);
 			draw_line(window_information['cmd'], 2, 63, 1, ACS_LLCORNER);
 			wmove(window_information['cmd'], 0, 5); waddstr(window_information['cmd'], " PROMPT ", color_pair(3));
+			update_panels();
+			doupdate();
 			
 			#Read the command...
 			command = mvwgetstr(window_information['cmd'], 1, 5);
